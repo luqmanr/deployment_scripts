fd227608fc2366eb476f22befca593aa2ed3df43
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2020-11-10 15:48:43 +0700

    core fr image sync script updated

--- api-recognition/database-sync/core-fr-img_sync.sh
+++ api-recognition/database-sync/core-fr-img_sync.sh
@@ -2,6 +2,8 @@
 config_file="database.env"
 set -a; source $config_file; set +a
 
-rsync -chavzP --delete-after -e "sshpass -p $cfr_pass ssh -p $remote_port" \
+# echo $cfr_pass, $cfr_port, $cfr_user, $cfr_ip, $cfr_program_path
+
+rsync -chavzP --delete-after -e "sshpass -p $cfr_pass ssh -p $cfr_port" \
 $cfr_user@$cfr_ip:$cfr_program_path/v1.0/media/ \
 $HOME/workspace/face-recognition/v1.0/media/ 

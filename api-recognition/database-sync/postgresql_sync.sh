216c47334afd0656ea6da4e9a4d563a186d4fff9
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2020-11-12 09:59:09 +0700

    latest ver sync scripts

--- api-recognition/database-sync/postgresql_sync.sh
+++ api-recognition/database-sync/postgresql_sync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-config_file="database.env"
+config_file="${BASH_SOURCE%/*}/database.env"
 set -a; source $config_file; set +a
+
+echo "syncing for database $bg_dbname"
 
 sshpass -p $bg_pass ssh -p $bg_port $bg_user@$bg_ip "PGPASSWORD=$bg_dbpass pg_dump \
 -U $bg_dbuser --format=custom -h localhost $bg_dbname" | \
-PGPASSWORD=$local_dbpass pg_restore --clean -U $local_dbuser -d $local_dbname
+PGPASSWORD=$local_dbpass pg_restore --clean -U $local_dbuser -d $local_dbname --verbose

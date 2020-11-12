fe1442abab55a7c344717ae8945ab30ec2bdc229
luqman <luqman.rahardjo@gmail.com>
2020-11-12 09:58:55 +0700

    mau pull

--- api-recognition/database-sync/postgresql_sync.sh
+++ api-recognition/database-sync/postgresql_sync.sh
@@ -2,6 +2,8 @@
 config_file="database.env"
 set -a; source $config_file; set +a
 
+echo "syncing for database $bg_dbname"
+
 sshpass -p $bg_pass ssh -p $bg_port $bg_user@$bg_ip "PGPASSWORD=$bg_dbpass pg_dump \
 -U $bg_dbuser --format=custom -h localhost $bg_dbname" | \
 PGPASSWORD=$local_dbpass pg_restore --clean -U $local_dbuser -d $local_dbname

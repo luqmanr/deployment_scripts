139ee472e8133c538ecd18cb69727cdd44af5a66
luqman <luqman.rahardjo@gmail.com>
2020-11-18 08:50:06 +0700

    edit core fr start

--- api-recognition/program-deployment/core-fr.start.sh
+++ api-recognition/program-deployment/core-fr.start.sh
@@ -2,8 +2,7 @@
 ## Start Docker Containers
 ${BASH_SOURCE%/*}/FR-Docker.start.sh
 ${BASH_SOURCE%/*}/SSD-Docker.start.sh
-
-sleep 10
+sleep 5
 
 ## Prepare tmux windows For Monitoring
 tmux new -s core-fr -d
@@ -13,8 +12,10 @@ do
 done
 
 ## Execute Programs Inside Docker Containers, Using tmux
-sleep 5
+# sleep 5
 tmux send-keys -t core-fr:1 "docker exec -it api-recognition python3 -m model_server.runserver lib/FeatureExtractor/serving.py" C-m
-tmux send-keys -t core-fr:2 "docker exec -it api-recognition python3 auto_train.py" C-m
-sleep 10
+tmux send-keys -t core-fr:2 "docker exec -it api-recognition python3 new_auto_training.py" C-m
+# sleep 10
 tmux send-keys -t core-fr:0 "docker exec -it api-recognition python3 fr_api.py" C-m
+sleep infinity
+tmux send-keys -t core-fr:3 "echo 'hello-world!'" C-m

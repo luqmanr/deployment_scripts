e132fd4bb1277879af6131cb6726b386f58acf72
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2020-11-18 13:23:11 +0700

    removed some scripts

--- sshfs/rkb3-sshfs.sh
+++
@@ -1,1 +0,0 @@
-echo adminRKB | sudo sshfs -o password_stdin,allow_other,default_permissions -p 23023 riset-ai@202.158.77.42:/ /mnt/rkb3-sshfs/
--- sshfs/rkb5-sshfs.sh
+++
@@ -1,1 +0,0 @@
-echo adminRKB | sudo sshfs -o password_stdin,allow_other,default_permissions -p 25025 ubuntu@202.158.77.42:/ /mnt/rkb5-sshfs/

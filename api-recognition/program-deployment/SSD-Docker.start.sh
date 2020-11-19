7613e05ba90c78d4b7f8b3c44d6174f7df46f1cf
luqman <luqman.rahardjo@gmail.com>
2020-11-19 15:23:06 +0700

    modified start scripts

--- api-recognition/program-deployment/SSD-Docker.start.sh
+++ api-recognition/program-deployment/SSD-Docker.start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 docker run --rm -it -d --gpus device=0 --name serving_ssd \
---cpus=".5" \
---memory="8g" \
---memory-swap="-1" \
 -v /home/ubuntu/workspace/face-recognition/recognition/lib/FaceDetector/ssd/model/serving/ssd/:/models/ssd \
 -v /home/ubuntu/workspace/face-recognition/recognition/lib/serving_configurations/models-ssd.conf:/models/models-ssd.conf \
 -t tensorflow/serving:latest-gpu --per_process_gpu_memory_fraction=0.3 --model_config_file=/models/models-ssd.conf
